6c2030bf2a0bf3b995476d1d436a7dfa6bb9fd46
xlocaste <irfandi.di122@gmail.com>
2025-05-11 20:12:13 +0700

    feat:admin panel store kerja sama industri

--- app/Http/Controllers/KerjaSamaIndustriController.php
+++ app/Http/Controllers/KerjaSamaIndustriController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\KerjaSamaIndustri\StoreRequest;
 use App\Models\KerjaSamaIndustri;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -28,5 +29,22 @@ class KerjaSamaIndustriController extends Controller
                 'daftarKSI' => $daftarKSI
             ]);
         }
+    }
+
+    public function store(StoreRequest $request)
+    {
+        KerjaSamaIndustri::create([
+            'nama_ksi'=>$request->nama_ksi,
+            'bidang_fokus_ksi'=>$request->bidang_fokus_ksi,
+            'tahun_ksi'=>$request->tahun_ksi,
+            'tahun_exit_ksi'=>$request->tahun_exit_ksi,
+        ]);
+
+        return redirect()->route('authentication.kerja-sama-industri.index');
+    }
+
+    public function create()
+    {
+        return Inertia::render('Authentication/KerjaSamaIndustri/Add');
     }
 }
