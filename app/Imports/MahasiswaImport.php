a19e0b9ffac9c7761518d4d74511404c7cd63804
xlocaste <irfandi.di122@gmail.com>
2025-05-23 21:35:09 +0700

    fix:error message import excel mahasiswa

--- app/Imports/MahasiswaImport.php
+++ app/Imports/MahasiswaImport.php
@@ -3,23 +3,31 @@
 namespace App\Imports;
 
 use App\Models\Mahasiswa;
-use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Row;
+use Maatwebsite\Excel\Concerns\OnEachRow;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class MahasiswaImport implements ToModel, WithHeadingRow
+class MahasiswaImport implements OnEachRow, WithHeadingRow
 {
-    /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
-    public function model(array $row)
+    public $errors = [];
+
+    public function onRow(Row $row)
     {
-        return new Mahasiswa([
-            'nama'     => $row['nama'],
-            'nim'      => $row['nim'],
-            'semester' => $row['semester'],
-            'ipk'      => $row['ipk'],
-        ]);
+        $data = $row->toArray();
+
+        try {
+            Mahasiswa::create([
+                'nama'     => $data['nama'],
+                'nim'      => $data['nim'],
+                'semester' => $data['semester'],
+                'ipk'      => $data['ipk'],
+            ]);
+        } catch (\Illuminate\Database\QueryException $e) {
+            if ($e->errorInfo[1] == 1062) {
+                $this->errors[] = "Duplikat NIM '{$data['nim']}' pada baris {$row->getIndex()}";
+            } else {
+                $this->errors[] = "Gagal menyimpan data pada baris {$row->getIndex()}: " . $e->getMessage();
+            }
+        }
     }
 }
